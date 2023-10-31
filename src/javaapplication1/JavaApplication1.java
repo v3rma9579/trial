d4a445e9872e4b519bceec30ab0344fcc1edb40a
Shubham Verma <shubhamverma286008@gmail.com>
2023-10-31 18:01:27 +0530

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author shubh
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RandomGame rg = new RandomGame();
+        rg.setVisible(true);
+    }
+    
+}
